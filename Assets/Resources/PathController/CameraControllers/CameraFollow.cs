192b764feeca4a827c065fa193bebab6c26040c6
Alexander-Stewart <stewart.stewie@yahoo.com>
2019-11-30 14:54:08 -0500

    saving changes

---
+++ Assets/Resources/PathController/CameraControllers/CameraFollow.cs
@@ -0,0 +1,38 @@
+﻿//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+//using PathCreation; 
+////This class will get the position of the camera based on a path it has been given, and will adapt
+////the text on the canvas according to the position in the path 
+
+
+//public class CameraFollow : MonoBehaviour
+//{
+//    public PathCreator pathCreator;
+//    public float speed = 5;
+//    public float distanceTravelled;
+//    //private Rigidbody rb; 
+
+//    // Update is called once per frame
+//    private void Start()
+//    {
+//        //rb = GetComponent<Rigidbody>(); 
+
+//    }
+
+//    void Update()
+//    {
+//        this.MoveAlongPath();
+
+//    }
+
+//    public void MoveAlongPath()
+//    { 
+//        distanceTravelled = speed * Time.time; 
+//        transform.position = pathCreator.path.GetPointAtDistance(distanceTravelled); 
+//        this.transform.rotation = pathCreator.path.GetRotationAtDistance(distanceTravelled);
+//        //rb.AddForce(transform.position); 
+
+
+//    }
+//}
---
+++ Assets/Resources/PathController/CameraControllers/CameraRotate.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRotate : MonoBehaviour
+{
+    public float speedX = 2.0f;
+    public float speedY = 2.0f;
+    private float rotateX = 0.0f;
+    private float rotateY = 0.0f; 
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        rotateX += speedX * Input.GetAxis("MouseX");
+        rotateY += speedY * Input.GetAxis("MouseY");
+
+        //transform.eulerAngles = new Vector3(rotateX, rotateY, 0.0f);
+        transform.Rotate(0, 0, rotateY); 
+
+    }
+}

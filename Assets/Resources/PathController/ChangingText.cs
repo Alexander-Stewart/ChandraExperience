b1455d8d5cf233ccdd19323eb89c8979241ab3ea
Alexander-Stewart <stewart.stewie@yahoo.com>
2019-12-01 20:43:55 -0500

    saving changes

--- Assets/Resources/PathController/ChangingText.cs
+++ Assets/Resources/PathController/ChangingText.cs
@@ -1,12 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI; 
+using UnityEngine.UI;
+using TMPro;
+
 
 public class ChangingText : MonoBehaviour
 {
 
-    public Text mytext = null;
+    public TextMeshProUGUI mytext = null;
     private string currentPathName;
     public MoveBetweenPoints tracker; 
     
@@ -28,62 +30,62 @@ public class ChangingText : MonoBehaviour
 
     }
 
-    public Text ChangeOutputText()
+    public void ChangeOutputText()
     {
         switch (currentPathName)
         {
             case "prepreOrbit":
-                mytext.text= "Hello from the Chandra X-Ray Observatory. Click anywhere on the screen to orbit the spacecraft and see it from all angles.  Clicking will move you along to another stop on the tour. ";
-                return mytext; 
+                mytext.text= "Hello from the Chandra X-Ray Observatory. The tour will start shortly. Please press the A button to return to the start of the experience. ";
+                break;
              
             case "preOrbit":
-                mytext.text= "Intro_ClickText";
-                return mytext; 
+                mytext.text= "";
+                break;
 
             case "tour6":
                 mytext.text= "Chandra is almost 14m long, about the size of a school bus.  It is only centimeters smaller than the largest payload the space shuttle could carry.";
-                return mytext;
+                break;
 
             case "tour1a":
                 mytext.text = "X-rays are too energetic to bounce off traditional mirrors like we use to see our reflection.";
-                return mytext;
+                break;
 
             case "tour1b":
-                mytext.text = "Instead, Chandra has nested\\n barrel-shaped mirrors that\\n allow the X-rays to skip\\n like a pebble across a pond\\n and then focus on the detector\\n 10 meters away.";
-                return mytext;
+                mytext.text = "Instead, Chandra has nested barrel-shaped mirrors that allow the X-rays to skip like a pebble across a pond and then focus on the detector 10 meters away.";
+                break;
 
             case "tour2":
-                mytext.text = "Chandra uses cameras and \\nspectrometers at its target\\n to analyze the X-rays\\n coming from deep space.";
-                return mytext;
+                mytext.text = "Chandra uses cameras and spectrometers at its target to analyze the X-rays coming from deep space.";
+                break;
 
             case "tour3a":
                 mytext.text = "Chandra's solar panels collect power for the telescope's detectors and its radio communication with the Earth.";
-                return mytext;
+                break;
 
             case "tour3b":
                 mytext.text = "The electricity is also used to heat the mirrors to keep them from deforming in the cold temperatures of space.";
-                return mytext;
+                break;
 
             case "tour4":
                 mytext.text = "In order to provide motion to the observatory, Chandra has two different sets of thrusters. Chandra aims with high-precision gyroscopes.";
-                return mytext;
+                break;
 
             case "tour5a":
                 mytext.text = "The antennas on Chandra are its link to NASA's Deep Space Network, a series of three radio dishes located at different parts of Earth.";
-                return mytext;
+                break;
 
             case "tour5b":
                 mytext.text = "Once on Earth, the system delivers the data to the Chandra X-ray Center, in Cambridge, Massachusetts.";
-                return mytext;
+                break;
 
             case "tour7":
                 mytext.text = "...";
-                return mytext;
+                break;
 
 
             default:
                 mytext.text= "Welcome to the Chandra X-Ray Observatory";
-                return mytext; 
+                break;
 
         }
     }

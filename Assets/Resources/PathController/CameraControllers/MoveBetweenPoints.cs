b22df08b83d4b87900aae233163542cff95c5284
Alexander-Stewart <stewart.stewie@yahoo.com>
2019-12-14 22:24:31 -0500

    we at a good point
    going to make it look better and going to add scene transitions

---
+++ Assets/Resources/PathController/CameraControllers/MoveBetweenPoints.cs
@@ -0,0 +1,131 @@
+﻿using System.Collections;
+
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events; 
+
+public class MoveBetweenPoints : MonoBehaviour
+{
+    private Pathway currentPath; 
+    public Rigidbody rb;
+    private Vector3 target;
+    private Tour tour;
+    //Creating a list of points that represent a pathway
+    private Vector3[] pathPoints;
+    private float lerpTime; 
+    private float currentLerpTime;
+    private Vector3 startPos;
+    float percentageComplete;
+    private bool isLerping;
+    private bool paused;
+
+    private Transform EarthHigh;
+    private Transform Chandra; 
+
+    private void Awake()
+    {
+        tour = gameObject.AddComponent<Tour>(); 
+      
+    }
+
+    void Start()
+    {
+        lerpTime = 10f; 
+        EarthHigh = GameObject.Find("ChandraOrbit").GetComponent<Transform>();
+        Chandra = EarthHigh.Find("folded-chandra").GetComponent<Transform>(); 
+        rb.GetComponent<Rigidbody>();
+        //isLerping = false;
+        //paused = false;
+        currentLerpTime = 0f;
+        StartCoroutine("SetTarget");
+
+    }
+
+
+    private IEnumerator SetTarget()
+    {
+        Debug.Log("starting SetTarget");
+        Debug.Log(Tour.pathways);
+        rb.transform.position = new Vector3(Tour.pathways[0].GetPoints()[0].x, Tour.pathways[0].GetPoints()[0].y, Tour.pathways[0].GetPoints()[0].z);
+
+        for (int p = 0; p < Tour.pathways.Count; p++)
+        {
+            currentPath = Tour.pathways[p];
+            Debug.Log("in tour, going to next path:   " + currentPath.GetName()+ "   :   " +  p);
+            for (int i = 0; i < currentPath.GetPointsLength()-1; i++)
+            {
+
+                startPos = new Vector3(currentPath.GetPoints()[i].x, currentPath.GetPoints()[i].y, currentPath.GetPoints()[i].z);
+                target = new Vector3(currentPath.GetPoints()[i+1].x, currentPath.GetPoints()[i+1].y, currentPath.GetPoints()[i+1].z);
+              
+                yield return new WaitForSeconds(lerpTime);
+                Debug.Log("going to next point: " + (i + 1));
+
+            }
+            startPos = currentPath.GetPoints()[currentPath.GetPointsLength() - 1];
+            target = currentPath.GetPoints()[currentPath.GetPointsLength() - 1];
+            yield return new WaitForSeconds(lerpTime);
+        }
+
+       
+        StartCoroutine("SetTarget");
+
+    }
+
+    private void Update()
+    {
+        MoveToTarget();
+        rb.transform.position = Chandra.TransformPoint(rb.transform.position);
+        //Debug.Log("chandra pos:   " + Chandra.transform.position); 
+
+    }
+
+
+    private void MoveToTarget()
+
+    {
+        //currentLerpTime is the time since the camera left the previous point 
+        //Interpolating between two the startPos and target defined in "Set Target" method 
+        isLerping = true;
+        currentLerpTime += Time.deltaTime;
+
+        //The currentLerpTime should not exceed the overall time needed to interpolate between two points 
+        if (currentLerpTime >= lerpTime)
+        {
+            currentLerpTime = lerpTime;
+        }
+
+
+        //Getting the interpolation step
+        percentageComplete = currentLerpTime / lerpTime;
+        rb.transform.position = Vector3.Lerp(startPos, target, percentageComplete);
+        if (percentageComplete >= 1.0)
+        {
+            Debug.Log("resetting current lerp time"); 
+            currentLerpTime = 0f;
+            isLerping = false;
+        }
+
+
+
+    }
+
+    private IEnumerator PauseAtPoint(float seconds)
+    {
+        if (!isLerping)
+        {
+            yield return new WaitForSeconds(seconds);
+       
+        }
+        print("pausing at point");
+        //yield return null;
+        //StartCoroutine(PauseAtPoint(pauseTime));
+
+    }
+
+    public Pathway GetCurrentPath()
+    {
+        return currentPath; 
+    }
+
+}
---
+++ Assets/Resources/PathController/ChangingText.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class ChangingText : MonoBehaviour
+{
+
+    public TextMeshProUGUI mytext = null;
+    private string currentPathName;
+    public MoveBetweenPoints tracker; 
+    
+     // Start is called before the first frame update
+    void Start()
+    {
+        //tracker= gameObject.A
+        //mytext = this.getWelcomeText();
+        //currentPathName = tracker.GetCurrentPath().pathName;
+
+
+    }
+
+    private void Update()
+    {
+        currentPathName = tracker.GetCurrentPath().pathName;
+        this.ChangeOutputText(); 
+
+
+    }
+
+    public TextMeshProUGUI ChangeOutputText()
+    {
+        switch (currentPathName)
+        {
+            case "prepreOrbit":
+                mytext.text= "Hello from the Chandra X-Ray Observatory. Click anywhere on the screen to orbit the spacecraft and see it from all angles.  Clicking will move you along to another stop on the tour. ";
+                return mytext; 
+             
+            case "preOrbit":
+                mytext.text= "Please enjoy the experience.";
+                return mytext; 
+
+            case "tour6":
+                mytext.text= "Chandra is almost 14m long, about the size of a school bus. It is only centimeters smaller than the largest payload the space shuttle could carry.";
+                return mytext;
+
+            case "tour1a":
+                mytext.text = "X-rays are too energetic to bounce off traditional mirrors like we use to see our reflection.";
+                return mytext;
+
+            case "tour1b":
+                mytext.text = "Instead, Chandra has nested barrel-shaped mirrors that allow the X-rays to skip like a pebble across a pond\\n and then focus on the detector 10 meters away.";
+                return mytext;
+
+            case "tour2":
+                mytext.text = "Chandra uses cameras and spectrometers at its target to analyze the X-rays coming from deep space.";
+                return mytext;
+
+            case "tour3a":
+                mytext.text = "Chandra's solar panels collect power for the telescope's detectors and its radio communication with the Earth.";
+                return mytext;
+
+            case "tour3b":
+                mytext.text = "The electricity is also used to heat the mirrors to keep them from deforming in the cold temperatures of space.";
+                return mytext;
+
+            case "tour4":
+                mytext.text = "In order to provide motion to the observatory, Chandra has two different sets of thrusters. Chandra aims with high-precision gyroscopes.";
+                return mytext;
+
+            case "tour5a":
+                mytext.text = "The antennas on Chandra are its link to NASA's Deep Space Network, a series of three radio dishes located at different parts of Earth.";
+                return mytext;
+
+            case "tour5b":
+                mytext.text = "Once on Earth, the system delivers the data to the Chandra X-ray Center, in Cambridge, Massachusetts.";
+                return mytext;
+
+            case "tour7":
+                mytext.text = "...";
+                return mytext;
+
+
+            default:
+                mytext.text= "Welcome to the Chandra X-Ray Observatory";
+                return mytext; 
+
+        }
+    }
+
+
+    //public Text getText()
+    //{
+    //    return mytext; 
+    //}
+
+
+    //Text getWelcomeText()
+    //{
+    //    mytext.text = "Welcome to the Chandra Experience"; 
+    //    return mytext; 
+    //}
+
+
+}
